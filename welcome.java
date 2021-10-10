23e97ddc289e4e31b645168d58d5ae778d270234
gumedam <89574614+gumedam@users.noreply.github.com>
2021-10-10 22:22:51 +0530

    Create welcome.java

---
+++ welcome.java
@@ -0,0 +1,11 @@
+//welcome.java
+
+public class welcome{
+
+	public static void main(String[] args) {
+		System.out.println("welcome guru");
+    System.out.println("welcome to bits pilani guru");
+	}
+
+}
+
